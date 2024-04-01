322d1e014b25816ceb3e13825a18928c9508f9ff
Klowden <klowdensolutions@gmail.com>
2024-04-01 02:50:59 -0400

    test

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	appDirectory: "app",
-	ignoredRouteFiles: ["**/.*"],
 	watchPaths: ["./public", "./.env"],
-	serverBuildTarget: "netlify",
-	server: "./server.js",
 	publicPath:
 		process.env.NODE_ENV === "production" ? "/<klowden.github.io>/" : "/",
 }
