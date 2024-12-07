483b505e0de3bc78c422191040abb7a72852fdc6
Klowden <klowdensolutions@gmail.com>
2024-12-06 21:28:12 -0500

    Cleaned autoposter

--- app/routes/portfolio.autoposter.tsx
+++ app/routes/portfolio.autoposter.tsx
@@ -1,30 +1,4 @@
-// import { SetStateAction, useState } from "react"
-// import { FaAngleLeft, FaAngleRight } from "react-icons/fa6"
-import imageData from "../data/resumeData.json"
-
 export default function AutoPoster() {
-	const exampleImages = imageData.ExampleImages
-
-	// // State to keep track of the current image index
-	// const [currentImageIndex, setCurrentImageIndex] = useState(0)
-
-	// // Function to go to the next image
-	// const nextImage = () => {
-	// 	setCurrentImageIndex((prevIndex) => (prevIndex + 1) % exampleImages.length)
-	// }
-
-	// // Function to go to the previous image
-	// const prevImage = () => {
-	// 	setCurrentImageIndex(
-	// 		(prevIndex) =>
-	// 			(prevIndex - 1 + exampleImages.length) % exampleImages.length
-	// 	)
-	// }
-
-	// const setImageIndex = (index: SetStateAction<number>) => {
-	// 	setCurrentImageIndex(index)
-	// }
-
 	return (
 		<div className='grid place-items-center'>
 			<div className=''>
@@ -101,59 +75,6 @@ export default function AutoPoster() {
 					</div>
 				</div>
 			</div>
-			{/* Image carousel container */}
-			{/* <div className='flex place-items-center mx-auto max-w-[60rem] h-[28rem] sm:h-[34rem] w-full px-4 pb-4 rounded-md gap-2'>
-				<FaAngleLeft
-					onClick={prevImage}
-					className='text-6xl cursor-pointer'
-				/>
-				<div className='w-full h-full flex flex-col place-content-center'>
-					<a
-						href={exampleImages[currentImageIndex].url}
-						target='_blank'
-						rel='noopener noreferrer'
-						className='flex relative justify-center font-bold text-amber-500 bg-gray-900 rounded-t-lg px-4 pb-4 pt-3'
-					>
-						<h3 className='flex w-max justify-center text-center text-xl sm:text-2xl md:text-3xl hover:text-blue-800 transition-all duration-150 ease-out hover:scale-[102%] active:scale-95'>
-							{exampleImages[currentImageIndex].title}
-						</h3>
-						<p className='absolute right-2 top-2 z-10 text-xs sm:text-base hover:text-blue-800 transition-all duration-150 ease-out hover:scale-[102%] active:scale-95'>
-							{currentImageIndex + 1}/{exampleImages.length}
-						</p>
-					</a>
-					<div className='place-items-center justify-center flex aspect-video w-full bg-slate-700 rounded-b-lg shadow-md p-4 gap-4'>
-						<img
-							src={exampleImages[currentImageIndex].src}
-							alt={exampleImages[currentImageIndex].alt}
-							className='h-auto max-h-80 sm:max-h-[28rem] object-contain rounded-lg shadow-md select-none'
-						/>
-					</div>
-				</div>
-				<FaAngleRight
-					onClick={nextImage}
-					className='text-6xl cursor-pointer'
-				/>
-			</div>
-			<div className='overflow-x-hidden p-4 max-w-full lg:max-w-[60rem]'>
-				<div className='grid grid-flow-col grid-rows-2 gap-4 p-4'>
-					{exampleImages.map((image, index) => (
-						<div
-							key={index}
-							className='cursor-pointer w-32 sm:w-36 flex flex-col'
-							onClick={() => setImageIndex(index)}
-						>
-							<h3 className='truncate text-sm text-center bg-black text-white rounded-t-md font-medium'>
-								{image.title}
-							</h3>
-							<img
-								src={image.src}
-								alt={image.alt}
-								className='w-full h-full aspect-square object-cover rounded-b-md shadow-lg hover:opacity-75 transition-opacity duration-150'
-							/>
-						</div>
-					))}
-				</div>
-			</div> */}
 		</div>
 	)
 }
